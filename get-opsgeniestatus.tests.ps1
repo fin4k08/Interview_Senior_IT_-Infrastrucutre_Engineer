a2a8587f6653311b340babcd1b537236af996aed
Andrew Findlay <fin4k08@live.co.uk>
2025-07-28 21:56:47 +0100

    added the unsorted list to pre pester mock

--- get-opsgeniestatus.tests.ps1
+++ get-opsgeniestatus.tests.ps1
@@ -68,15 +68,10 @@ Status: resolved
             $slackMessage | Should -Match "Current systems status: Partially Degraded"
             Write-Host "$slackMessage"
         }
-        
+
         It "Should sort incidents in reverse chronological order by created_at" {
             $sampleIncidents = @{
                 incidents = @(
-                    @{
-                        name = "Issue with website login"
-                        created_at = "2019-09-05T08:32:51.523Z"
-                        status = "unresolved"
-                    },
                     @{
                         name = "Alerts are very slow to send"
                         created_at = "2019-07-19T22:04:01.109Z"
@@ -86,11 +81,17 @@ Status: resolved
                         name = "Some users receiving duplicate alerts"
                         created_at = "2019-06-27T03:16:31.647Z"
                         status = "resolved"
+                    },
+                    @{
+                        name = "Issue with website login"
+                        created_at = "2019-09-05T08:32:51.523Z"
+                        status = "unresolved"
                     }
                 )
             }
 
             # Sort using same logic as the script
+            Write-Host "$($sampleIncidents.incidents.created_at)" -foregroundcolor magenta
             $sorted = $sampleIncidents.incidents |
                 Sort-Object { [datetime]$_.created_at } -Descending
 
